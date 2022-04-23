7e1375c18f37e4c3bdf61472bbb78eb87acb50ca
xiaomeng <764504466@qq.com>
2022-04-23 16:13:38 +0800

    转HTML以完成

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js'
-  }
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'app1',
+    template: './src/assets/test.html'
+  })]
 };
