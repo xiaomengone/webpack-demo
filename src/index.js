4bb3996ce0e0cfc703d899f5a6d8812d8526bae6
xiaomeng <764504466@qq.com>
2022-04-23 09:54:19 +0800

    innit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import x from './x.js'
+console.log(x);
+console.log('c');
+console.log('hi');
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+var path = require('path');
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js'
+  }
+};
